68749aaa140d4c192181b0dc2501b16da9627239
Kunal <kunalbhat6767@gmail.com>
2023-11-26 12:31:16 +0530

    LifeCycle Rule

--- main.tf
+++ main.tf
@@ -18,8 +18,7 @@ provider "aws" {
 
 }
 
-
-
+# EC2 instance
 resource "aws_instance" "terraformInstance" {
 
   ami           = "ami-05c13eab67c5d8861"
@@ -28,8 +27,39 @@ resource "aws_instance" "terraformInstance" {
     Name = "terraformGettingStarted"
   }
   vpc_security_group_ids = [aws_security_group.terraformSG.id]
+  # connection {
+  #   type        = "ssh"
+  #   user        = "ec2-user"
+  #   private_key = file("C:/Users/kunal/Desktop/terraform.pem")
+  #   host        = self.public_ip
+  # }
 
 }
+resource "aws_s3_bucket" "Bucket" {
+  
+  bucket = "terraform-bucket-123456789"
+  
+}
+resource "aws_s3_bucket_lifecycle_configuration" "BucketLifecycle" {
+  
+  bucket = aws_s3_bucket.Bucket.id
+  rule {
+    status = "Enabled"
+    id = "Rule-1"
+    expiration {
+      days = 80
+    }
+  }
+   
+  
+}
+
+# VPC
+resource "aws_vpc" "VPC" {
+  cidr_block = "192.168.0.0/16"
+}
+
+# Security Group
 resource "aws_security_group" "terraformSG" {
 
   name        = "terraformSG"
@@ -39,8 +69,28 @@ resource "aws_security_group" "terraformSG" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+  vpc_id = aws_vpc.VPC.id
+  lifecycle {
+    create_before_destroy = true
   }
 }
+
+# IGW
+resource "aws_internet_gateway" "IGW" {
+  vpc_id = aws_vpc.VPC.id
+}
+
+# Subnet
+resource "aws_subnet" "SUBNET" {
+ 
+ vpc_id = aws_vpc.VPC.id  
+ map_public_ip_on_launch = true
+  
+}
+
+# EBS
 resource "aws_ebs_volume" "terraformEBS" {
 
   availability_zone = "us-east-1a"
@@ -49,6 +99,7 @@ resource "aws_ebs_volume" "terraformEBS" {
     name        = "terraformEBS"
     description = "EBS volume created from terraform"
   }
+  
   
 }
 
@@ -66,5 +117,9 @@ output "AMI_ID" {
   description = "value of the AMI id"
 
 }
+output "EBS_ID" {
 
+  value       = aws_ebs_volume.terraformEBS.id
+  description = "value of the EBS id"
 
+}
